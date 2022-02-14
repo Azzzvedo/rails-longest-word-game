b0d41253dea468885bc13420ba2eb669e9c08d4e
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-02-14 22:49:00 +0000

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,22 +4,34 @@ require "open-uri"
 class GamesController < ApplicationController
 
   def new
-
     @game_letters = Array.new(10) { ('a'..'z').to_a.sample }
+    @starting = Time.now
   end
 
   def score
     @word = params[:attempt]
+    @time = (Time.now - Time.parse(params[:starting])).to_f
     @game_letters = params[:game_letters].split
-    @response = if correct_letters?(@word, @game_letters)
-                  if exists?(@word)
-                    'banging word bro!'
-                  else
-                    "Sorry but <strong>#{@word}</strong> doesn't seem to be a valid English word..."
-                  end
-                else
-                  "Sorry but <strong>#{@word}</strong> can't be built out of #{@game_letters.join(', ')} "
-                end
+    @response = message(@word, @game_letters, @time)
+  end
+
+  def message(word, letters, time)
+    if correct_letters?(word, letters)
+      if exists?(word)
+        @score = score_value(time, word.length)
+        "banging word bro! You scored #{@score}"
+      else
+        @score = 0
+        "Sorry but <strong>#{word}</strong> doesn't seem to be a valid English word..."
+      end
+    else
+      @score = 0
+      "Sorry but <strong>#{word}</strong> can't be built out of #{letters.join(', ')} "
+    end
+  end
+
+  def score_value(time, length)
+    length**(time * -1)
   end
 
   def exists?(word)
@@ -32,4 +44,8 @@ class GamesController < ApplicationController
     word_array = word.downcase.chars
     word_array.all? { |l| word_array.count(l) <= compare_letters.count(l) }
   end
+
+
+  # time consuming operation
+
 end
